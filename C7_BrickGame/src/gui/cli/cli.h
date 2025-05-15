9a30913d331a23e5bebf794037c49be3953fdd9d
Danila Mironov <par.partner2013@yandex.ru>
2025-05-16 01:00:19 +0300

    Update cli.h

--- C7_BrickGame/src/gui/cli/cli.h
+++ C7_BrickGame/src/gui/cli/cli.h
@@ -1,23 +1,54 @@
 #ifndef CLI_H
 #define CLI_H
 
-#include "../../brick_game/tetris/backend.h"
+#include <stdbool.h>
+
+#define BOARD_HEIGHT 20
+#define BOARD_WIDTH 10
+#define FIGURE_SIZE 4
 
 #define WINDOW_TOP 1
-#define WINDOW_LEFT 7
-#define WINDOW_HEIGHT 24
-#define WINDOW_WIDTH 41
+#define WINDOW_LEFT 1
+#define FIELD_HEIGHT BOARD_HEIGHT
+#define FIELD_WIDTH BOARD_WIDTH
+#define WINDOW_WIDTH 80
 
-#define FIELD_HEIGHT 22
-#define FIELD_WIDTH 24
+typedef struct {
+    int board[BOARD_HEIGHT][BOARD_WIDTH];         // Игровое поле
+    int next_figure[FIGURE_SIZE][FIGURE_SIZE];    // Следующая фигура
+    int current_figure[FIGURE_SIZE][FIGURE_SIZE]; // Текущая фигура
+    int current_x;                                 // Координата X текущей фигуры
+    int current_y;                                 // Координата Y текущей фигуры
+    int level;                                    // Текущий уровень
+    int score;                                    // Текущий счет
+    int high_score;                               // Рекордный счет
+    bool paused;                                  // Флаг паузы
+    bool game_started;                            // Флаг начала игры
+} game_info_t;
 
+// Инициализация ncurses и графического интерфейса
 void init_gui(void);
+
+// Отрисовка всего игрового окна и интерфейса
 void draw_game(const game_info_t *game);
+
+// Отрисовка окна (границ)
 void draw_window(void);
+
+// Отрисовка прямоугольника с заданными координатами (границы окна и панелей)
 void draw_rectangle(int top, int bottom, int left, int right);
+
+// Отрисовка игровой информации (уровень, счет, пауза, инструкции)
 void draw_info(const game_info_t *game);
+
+// Отрисовка следующей фигуры
 void draw_next(const int **next_figure);
+
+// Отрисовка игрового поля
 void draw_field(const int **board);
+
+// Отрисовка текущей активной фигуры
 void draw_current(const game_info_t *game);
 
-#endif
+#endif // CLI_H
+
